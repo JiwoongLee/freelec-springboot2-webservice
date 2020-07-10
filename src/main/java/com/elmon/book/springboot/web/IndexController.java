678cc0f27f1fea9170366da5e8bce02d8dbc22e2
JiwoongLee <rahad8688@gmail.com>
2020-07-10 19:50:00 +0900

    improvement login based on annotation

--- src/main/java/com/elmon/book/springboot/web/IndexController.java
+++ src/main/java/com/elmon/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.elmon.book.springboot.web;
 
+import com.elmon.book.springboot.config.auth.LoginUser;
 import com.elmon.book.springboot.config.auth.dto.SessionUser;
 import com.elmon.book.springboot.service.posts.PostsService;
 import com.elmon.book.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,12 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+    //private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
