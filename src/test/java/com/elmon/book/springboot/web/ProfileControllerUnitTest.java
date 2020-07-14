abae2ae027178ff1b1b61cd9cc7c3571bc565b06
JiwoongLee <rahad8688@gmail.com>
2020-07-14 18:20:45 +0900

    add profile api and test code

---
+++ src/test/java/com/elmon/book/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,59 @@
+package com.elmon.book.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile_lookup() {
+        //given
+        String expectedProfie = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfie);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfie);
+    }
+
+    @Test
+    public void real_profile_is_no_first_lookup() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_profile_is_no_default_lookup() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
